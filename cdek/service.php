f459607fd8b29abab46a30308d735a6693731aed
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-10-22 11:13:01 +0400

    fix

--- cdek/service.php
+++ cdek/service.php
@@ -11,7 +11,7 @@ use infrajs\cart\cdek\ISDEKservice;
 
 ISDEKservice::setTarifPriority(
 	array(233, 137, 139, 16, 18, 11, 1, 3, 61, 60, 59, 58, 57, 83),
-    array(234, 136, 138, 15, 17, 10, 12, 5, 62, 63)
+    array(234, 136, 138, 62, 15, 17, 10, 12, 5, 63)
 );
 
 $action = $_REQUEST['isdek_action'];
