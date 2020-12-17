ff88a550c9fddf70d4ad92ebb565391b149f3d33
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-12-17 12:21:35 +0400

    fix

--- api/handlers.php
+++ api/handlers.php
@@ -91,7 +91,7 @@ $context->handlers = [
 	},
 	
 	"create_order_user" => function () {
-		extract($this->gets(['ans','order', 'user', 'lang']));
+		extract($this->gets(['ans','order', 'user', 'lang', 'city_id','timezone']));
 		
 		$email = $order['email'];
 		//$order['user'] - это тот кому принадлежит заказ, может отличаться от указанного email
@@ -106,7 +106,7 @@ $context->handlers = [
 			$ans['token'] = $ouser['user_id'] . '-' . $ouser['token'];
 			
 			$r = Cart::setOwner($order['order_id'], $ouser['user_id']);
-			if (!$r) return $this->fail('CR018.e1');
+			if (!$r) return $this->fail('CR018');
 		} else { //Если на указанный email уже есть регистрация
 			if ($ouser['user_id'] != $user['user_id']) { //и это не мы
 				
@@ -120,12 +120,16 @@ $context->handlers = [
 					//У текущего пользователя есть аккаунт, ему не надо переходить, но нужно дать доступ и пользователю по указанному email
 					//Передаём доступ к текущему заказу и на этот аккаунт и делает для нового аккаунта этот заказ активным?. Злоумышленник кому-то может подсунуть новый заказ)
 					Cart::setOwner($order['order_id'], $ouser['user_id']);
-					if (!$r) return $this->fail('CR018.e2');
+					if (!$r) return $this->fail('CR018');
 					//И пропускаем оформление заказа дальше
 				}
 				
+			} else {
+				$r = Cart::setOwner($order['order_id'], $ouser['user_id']);
+				if (!$r) return $this->fail('CR018');	
 			}
-		}
+		}	
+		
 		Cart::recalc($order['order_id']);
 		$order = Cart::getById($order['order_id']);
 	}
